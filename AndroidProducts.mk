a6501e25196a2ff1114fe092b6963e1ca4eb7e6b
ycly2333 <2780870183@qq.com>
2022-02-06 21:57:53 +0800

    H9S

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_H9S.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_H9S-user \
+    omni_H9S-userdebug \
+    omni_H9S-eng
---
+++ omni_H9S.mk
@@ -0,0 +1,9 @@
+# Inherit from H9S device
+$(call inherit-product, device/EEBBK/H9S/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := H9S
+PRODUCT_NAME := omni_H9S
+PRODUCT_BRAND := EEBBL
+PRODUCT_MODEL := H9S
+PRODUCT_MANUFACTURER := EEBBK
